e4f3a194e09b01b49f29b766e4b6ccbbb3827bcf
Markus Ast <m@rkusa.st>
2015-12-23 11:00:14 +0100

    test Vec4 add itself

--- scalar/vec4_test.go
+++ scalar/vec4_test.go
@@ -8,10 +8,16 @@ import (
 func TestAdd(t *testing.T) {
 	lhs := &Vec4{1, 2, 3, 4}
 	rhs := &Vec4{5, 6, 7, 8}
-
 	lhs.Add(rhs)
 	if !reflect.DeepEqual(lhs, &Vec4{6, 8, 10, 12}) {
 		t.Fatalf("Add wrong result, got: %v", lhs)
+	}
+
+	// test add itself
+	lhs = &Vec4{1, 2, 3, 4}
+	lhs.Add(lhs)
+	if !reflect.DeepEqual(lhs, &Vec4{2, 4, 6, 8}) {
+		t.Fatalf("Add itself wrong result, got: %v", lhs)
 	}
 }
 
@@ -24,10 +30,10 @@ func BenchmarkAdd(b *testing.B) {
 		lhs.Add(rhs)
 	}
 }
+
 func TestMul(t *testing.T) {
 	lhs := &Vec4{1, 2, 3, 4}
 	var rhs float32 = 2.5
-
 	lhs.Mul(rhs)
 	if !reflect.DeepEqual(lhs, &Vec4{2.5, 5, 7.5, 10}) {
 		t.Fatalf("Mul wrong result, got: %v", lhs)
