a9bdbc95ee7999972fe44b0b7a1b51dc05da2fd5
Markus Ast <m@rkusa.st>
2015-12-26 21:58:27 +0100

    remove assembly from untested architectures

--- math32/tan_amd64p32.s
+++
@@ -1,9 +0,0 @@
-// This file contains a subset of functions of the std
-// math library from Go, but converted from float64 to float32.
-
-// Copyright 2013 The Go Authors.  All rights reserved.
-// Use of this source code is governed by a BSD-style
-// license that can be found in the LICENSE file.
-
-#include "tan_amd64.s"
-
