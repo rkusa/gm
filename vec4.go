3abdd5df85ac0fedec86605c5e6897d7907022bb
Markus Ast <m@rkusa.st>
2015-12-28 14:19:29 +0100

    implement Vec4.Clone() & Vec4.Transform()

--- vec4.go
+++ vec4.go
@@ -19,6 +19,11 @@ func (lhs *Vec4) Add(rhs *Vec4) *Vec4 {
 	return lhs
 }
 
+// Clone the vector.
+func (lhs *Vec4) Clone() *Vec4 {
+	return &Vec4{lhs[0], lhs[1], lhs[2], lhs[3]}
+}
+
 func divVec4SIMD(lhs *Vec4, rhs float32)
 
 func divVec4(lhs *Vec4, rhs float32) {
@@ -76,5 +81,16 @@ func subVec4(lhs, rhs *Vec4) {
 // Sub substracts two vectors. Returns itself for function chaining.
 func (lhs *Vec4) Sub(rhs *Vec4) *Vec4 {
 	subVec4(lhs, rhs)
+	return lhs
+}
+
+// Transform the vector using a 4x4 matrix. Returns itself for function
+// chaining.
+func (lhs *Vec4) Transform(rhs *Mat4) *Vec4 {
+	x, y, z, w := lhs[0], lhs[1], lhs[2], lhs[3]
+	lhs[0] = rhs[0]*x + rhs[4]*y + rhs[8]*z + rhs[12]*w
+	lhs[1] = rhs[1]*x + rhs[5]*y + rhs[9]*z + rhs[13]*w
+	lhs[2] = rhs[2]*x + rhs[6]*y + rhs[10]*z + rhs[14]*w
+	lhs[3] = rhs[3]*x + rhs[7]*y + rhs[11]*z + rhs[15]*w
 	return lhs
 }
