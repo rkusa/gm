8681d004a6db55a5b1d3d9215ee69484bc3e5ec7
Markus Ast <m@rkusa.st>
2016-02-03 14:23:56 +0100

    fix invert arch API differentiation

--- mat4/invert.go
+++ mat4/invert.go
@@ -1,5 +1,15 @@
 package mat4
 
+// Invert the matrix. Returns nil if inversion is not possible due to a zero
+// row; otherwise, return itself for function chaining.
+func (lhs *Mat4) Invert() *Mat4 {
+	if !invertSIMD(lhs) {
+		return nil
+	}
+
+	return lhs
+}
+
 func invert(out *Mat4) bool {
 	lhs := Mat4{
 		out[0], out[1], out[2], out[3],
